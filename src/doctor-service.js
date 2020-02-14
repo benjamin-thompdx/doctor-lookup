700d14516da085a6514b67d668ad5d11b126d303
Benjamin Thom <jaminthom@gmail.com>
2020-02-14 13:15:25 -0800

    experiement with data input types and API call requests

--- src/doctor-service.js
+++ src/doctor-service.js
@@ -1,7 +1,7 @@
 export class DinoService {
   async getDinoIpsum(input1, input2) {
     try {
-      let response = await fetch(`http://dinoipsum.herokuapp.com/api/?format=json&paragraphs=${input1}&words=${input2}`);
+      let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?name=${input1}&query=${input2}&location=or-portland&skip=0&limit=10&user_key=${process.env.API_KEY}`);
       let jsonifiedResponse;
       if (response.ok && response.status === 200) {
         jsonifiedResponse = await response.json();
