7620489819389de1fb0c08cea07536093097fe6f
Benjamin Thom <jaminthom@gmail.com>
2020-02-14 14:27:37 -0800

    remove input2, update input1 to be called doctorName update button class to showDoctor, and update container to DoctorService within index.html, doctor-service.js, and main.js

--- src/main.js
+++ src/main.js
@@ -2,29 +2,27 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
-import { DinoService } from './../src/doctor-service.js';
+import { DoctorService } from './../src/doctor-service.js';
 
 $(document).ready(function() {
 
-  $('#getDinos').click(function() {
-    const input1 = $('#paragraphs').val();
-    $('#paragraphs').val("");
-    const input2 = $('#words').val();
-    $('#words').val("");
+  $('#findDoctor').click(function() {
+    const name = $('#doctorName').val();
+    $('#doctorName').val("");
 
     (async () => {
-      let dinoService = new DinoService();
-      const response = await dinoService.getDinoIpsum(input1, input2);
+      let doctorService = new DoctorService();
+      const response = await doctorService.getDocByName(name);
       getElements(response);
     })();
 
     function getElements(response) {
       if (response) {
-        $('.showDinos').text(`${response}`);
-        $('.showDinos').show();
+        $('.showDoctors').text(`${response}`);
+        $('.showDoctors').show();
       } else {
-        $('.showDinos').text(`There was an error handeling your request.`);
-        $('.showDinos').show();
+        $('.showDoctors').text(`There was an error handeling your request.`);
+        $('.showDoctors').show();
       }
     }
 
