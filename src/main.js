6d8b872d0f8256192e6fc83376c040472e3dd9be
Benjamin Thom <jaminthom@gmail.com>
2020-02-15 12:43:51 -0800

    update table in html.index to include first name, last name, address, phone number, website, and accepting new patients. update main.js to include outputs to display first name, last name, address, phone number, website, and accepting new patients within the DOM

--- src/main.js
+++ src/main.js
@@ -14,29 +14,36 @@ $(document).ready(function() {
       let doctorService = new DoctorService();
       const response = await doctorService.getDocByName(name);
       getElements(response);
+      console.log(response);
     })();
 
     function getElements(response) {
       if (response) {
         let table = document.getElementById('showByName');
         response.data.forEach(function (value, i) {
-          // console.log('index: ', i);
-          // console.log('value: ', value);
-          // console.log('first-name: ', value.profile.first_name);
           let row = table.insertRow(i+1);
-
           let firstNameCell = row.insertCell(0);
           let lastNameCell = row.insertCell(1);
+          let addressCell = row.insertCell(2);
+          let phoneCell = row.insertCell(3);
+          let websiteCell = row.insertCell(4);
+          let acceptingNewPatientsCell = row.insertCell(5);
 
           firstNameCell.innerHTML = value.profile.first_name;
+          
           lastNameCell.innerHTML = value.profile.last_name;
-        });
+          
+          addressCell.innerHTML = value.practices[0].visit_address.street + "," + " " + value.practices[0].visit_address.street2 + "," + " " + value.practices[0].visit_address.city + "," + " " + value.practices[0].visit_address.state + "," + " " + value.practices[0].visit_address.zip;
+          
+          phoneCell.innerHTML = value.practices[0].phones[0].number + "," + " " + value.practices[0].phones[0].type;
 
-        // $('#showByName').text(`${response.data[0].profile.first_name}`);
-        // $('#showByName').show();
+          websiteCell.innerHTML = 'https://BetterDocotor.com';
+          
+          acceptingNewPatientsCell.innerHTML = value.practices[0].accepts_new_patients;
+
+        });
       } else {
-        $('#showByName').text(`There was an error handeling your request.`);
-        $('#showByName').show();
+        $(".errors").text(`There was an error handeling your request.`);
       }
     }
 
