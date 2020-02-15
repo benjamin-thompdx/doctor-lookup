d2d227d0cdfa719f2228bae5ad7555578e4c08ca
Benjamin Thom <jaminthom@gmail.com>
2020-02-15 13:13:22 -0800

    update input id within index.html from doctorName to userInput, update all instances of input id within main.js to userInput

--- src/doctor-service.js
+++ src/doctor-service.js
@@ -13,4 +13,18 @@ export class DoctorService {
       return false;
     }
   }
+  async getDocBySymptom(symptom) {
+    try {
+      let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?query=${symptom}&location=or-portland&skip=0&limit=10&user_key=${process.env.API_KEY}`);
+      let jsonifiedResponse;
+      if (response.ok && response.status === 200) {
+        jsonifiedResponse = await response.json();
+      } else {
+        jsonifiedResponse = false;
+      }
+      return jsonifiedResponse;
+    } catch (error) {
+      return false;
+    }
+  }
 }
--- src/main.js
+++ src/main.js
@@ -7,14 +7,13 @@ import { DoctorService } from './../src/doctor-service.js';
 $(document).ready(function() {
 
   $('#findDoctorByName').click(function() {
-    const name = $('#doctorName').val();
-    $('#doctorName').val("");
+    const name = $('#userInput').val();
+    $('#userInput').val("");
 
     (async () => {
       let doctorService = new DoctorService();
       const response = await doctorService.getDocByName(name);
       getElements(response);
-      console.log(response);
     })();
 
     function getElements(response) {
@@ -30,23 +29,50 @@ $(document).ready(function() {
           let acceptingNewPatientsCell = row.insertCell(5);
 
           firstNameCell.innerHTML = value.profile.first_name;
-          
           lastNameCell.innerHTML = value.profile.last_name;
-          
           addressCell.innerHTML = value.practices[0].visit_address.street + "," + " " + value.practices[0].visit_address.street2 + "," + " " + value.practices[0].visit_address.city + "," + " " + value.practices[0].visit_address.state + "," + " " + value.practices[0].visit_address.zip;
-          
           phoneCell.innerHTML = value.practices[0].phones[0].number + "," + " " + value.practices[0].phones[0].type;
-
           websiteCell.innerHTML = 'https://BetterDocotor.com';
-          
           acceptingNewPatientsCell.innerHTML = value.practices[0].accepts_new_patients;
-
         });
       } else {
         $(".errors").text(`There was an error handeling your request.`);
       }
     }
-
   });
 
+  $('#findDoctorBySymptom').click(function() {
+    const symptom = $('#userInput').val();
+    $('#userInput').val("");
+
+    (async () => {
+      let doctorService = new DoctorService();
+      const response = await doctorService.getDocBySymptom(symptom);
+      getElements(response);
+    })();
+
+    function getElements(response) {
+      if (response) {
+        let table = document.getElementById('showByName');
+        response.data.forEach(function (value, i) {
+          let row = table.insertRow(i+1);
+          let firstNameCell = row.insertCell(0);
+          let lastNameCell = row.insertCell(1);
+          let addressCell = row.insertCell(2);
+          let phoneCell = row.insertCell(3);
+          let websiteCell = row.insertCell(4);
+          let acceptingNewPatientsCell = row.insertCell(5);
+
+          firstNameCell.innerHTML = value.profile.first_name;
+          lastNameCell.innerHTML = value.profile.last_name;
+          addressCell.innerHTML = value.practices[0].visit_address.street + "," + " " + value.practices[0].visit_address.street2 + "," + " " + value.practices[0].visit_address.city + "," + " " + value.practices[0].visit_address.state + "," + " " + value.practices[0].visit_address.zip;
+          phoneCell.innerHTML = value.practices[0].phones[0].number + "," + " " + value.practices[0].phones[0].type;
+          websiteCell.innerHTML = 'https://BetterDocotor.com';
+          acceptingNewPatientsCell.innerHTML = value.practices[0].accepts_new_patients;
+        });
+      } else {
+        $(".errors").text(`There was an error handeling your request.`);
+      }
+    }
+  });
 });
