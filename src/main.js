8de80b76c2441c92cba4b5a883533918f0608fc3
Benjamin Thom <jaminthom@gmail.com>
2020-02-16 14:49:54 -0800

    update main.js to include a user prompt for when no doctors are found from a search for locating a doctor by name and locating a doctor by symptom, update styling within styles.css

--- src/main.js
+++ src/main.js
@@ -19,23 +19,27 @@ $(document).ready(function() {
 
     function getElements(response) {
       if (response) {
-        let table = document.getElementById('userOutput');
-        response.data.forEach(function (value, i) {
-          let row = table.insertRow(i+1);
-          let firstNameCell = row.insertCell(0);
-          let lastNameCell = row.insertCell(1);
-          let addressCell = row.insertCell(2);
-          let phoneCell = row.insertCell(3);
-          let websiteCell = row.insertCell(4);
-          let acceptingNewPatientsCell = row.insertCell(5);
-
-          firstNameCell.innerHTML = value.profile.first_name;
-          lastNameCell.innerHTML = value.profile.last_name;
-          addressCell.innerHTML = value.practices[0].visit_address.street + "," + " " + value.practices[0].visit_address.street2 + "," + " " + value.practices[0].visit_address.city + "," + " " + value.practices[0].visit_address.state + "," + " " + value.practices[0].visit_address.zip;
-          phoneCell.innerHTML = value.practices[0].phones[0].number + "," + " " + value.practices[0].phones[0].type;
-          websiteCell.innerHTML = 'https://BetterDocotor.com';
-          acceptingNewPatientsCell.innerHTML = value.practices[0].accepts_new_patients;
-        });
+        if(response.data.length > 0) {
+          let table = document.getElementById('userOutput');
+          response.data.forEach(function (value, i) {
+            let row = table.insertRow(i+1);
+            let firstNameCell = row.insertCell(0);
+            let lastNameCell = row.insertCell(1);
+            let addressCell = row.insertCell(2);
+            let phoneCell = row.insertCell(3);
+            let websiteCell = row.insertCell(4);
+            let acceptingNewPatientsCell = row.insertCell(5);
+  
+            firstNameCell.innerHTML = value.profile.first_name;
+            lastNameCell.innerHTML = value.profile.last_name;
+            addressCell.innerHTML = value.practices[0].visit_address.street + "," + " " + value.practices[0].visit_address.street2 + "," + " " + value.practices[0].visit_address.city + "," + " " + value.practices[0].visit_address.state + "," + " " + value.practices[0].visit_address.zip;
+            phoneCell.innerHTML = value.practices[0].phones[0].number + "," + " " + value.practices[0].phones[0].type;
+            websiteCell.innerHTML = 'https://BetterDocotor.com';
+            acceptingNewPatientsCell.innerHTML = value.practices[0].accepts_new_patients;
+          });
+        } else {
+          $(".noDoctors").text('No doctors found based on your search');
+        }
       } else {
         $(".errors").text(`There was an error handling your request`);
       }
@@ -54,23 +58,27 @@ $(document).ready(function() {
 
     function getElements(response) {
       if (response) {
-        let table = document.getElementById('userOutput');
-        response.data.forEach(function (value, i) {
-          let row = table.insertRow(i+1);
-          let firstNameCell = row.insertCell(0);
-          let lastNameCell = row.insertCell(1);
-          let addressCell = row.insertCell(2);
-          let phoneCell = row.insertCell(3);
-          let websiteCell = row.insertCell(4);
-          let acceptingNewPatientsCell = row.insertCell(5);
-
-          firstNameCell.innerHTML = value.profile.first_name;
-          lastNameCell.innerHTML = value.profile.last_name;
-          addressCell.innerHTML = value.practices[0].visit_address.street + "," + " " + value.practices[0].visit_address.street2 + "," + " " + value.practices[0].visit_address.city + "," + " " + value.practices[0].visit_address.state + "," + " " + value.practices[0].visit_address.zip;
-          phoneCell.innerHTML = value.practices[0].phones[0].number + "," + " " + value.practices[0].phones[0].type;
-          websiteCell.innerHTML = 'https://BetterDocotor.com';
-          acceptingNewPatientsCell.innerHTML = value.practices[0].accepts_new_patients;
-        });
+        if(response.data.length > 0) {
+          let table = document.getElementById('userOutput');
+          response.data.forEach(function (value, i) {
+            let row = table.insertRow(i+1);
+            let firstNameCell = row.insertCell(0);
+            let lastNameCell = row.insertCell(1);
+            let addressCell = row.insertCell(2);
+            let phoneCell = row.insertCell(3);
+            let websiteCell = row.insertCell(4);
+            let acceptingNewPatientsCell = row.insertCell(5);
+  
+            firstNameCell.innerHTML = value.profile.first_name;
+            lastNameCell.innerHTML = value.profile.last_name;
+            addressCell.innerHTML = value.practices[0].visit_address.street + "," + " " + value.practices[0].visit_address.street2 + "," + " " + value.practices[0].visit_address.city + "," + " " + value.practices[0].visit_address.state + "," + " " + value.practices[0].visit_address.zip;
+            phoneCell.innerHTML = value.practices[0].phones[0].number + "," + " " + value.practices[0].phones[0].type;
+            websiteCell.innerHTML = 'https://BetterDocotor.com';
+            acceptingNewPatientsCell.innerHTML = value.practices[0].accepts_new_patients;
+          });
+        } else {
+          $(".noDoctors").text('No doctors found based on your search');
+        }
       } else {
         $(".errors").text(`There was an error handling your request`);
       }
